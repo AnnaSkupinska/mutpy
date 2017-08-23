48a3a88f7048bcb502ccedf036127c102d44211f
AnnaSkupinska <annaskupinska3@gmail.com>
2017-08-23 18:29:50 +0200

    Written comments for Mutation Controller Class

--- mutpy/controller.py
+++ mutpy/controller.py
@@ -47,6 +47,15 @@ class MutationScore:
 
 class MutationController(views.ViewNotifier):
 
+    # Initialises Mutation Controller
+    # target_loader - utils.ModulesLoader type. Provides access to target modules
+    # test_loader - utils.ModulesLoader type. Provides access to test modules
+    # views - list of object from classes in view.py. They allow communication with the user.
+    # mutant_generator - controller.HighOrderMutator or controller.FirstOrderMutator type. Used to mutate modules.
+    # timeout_factor - integer. Decides how long mutants are tested before being discarded.
+    # disable_stdout - boolean. Disables stdout if true.
+    # mutate_covered - boolean. Activates coverage injection if true.
+    # mutation_number - None or integer. Amount of mutations per one mutant. None means standard one mutation
     def __init__(self, target_loader, test_loader, views, mutant_generator,
                  timeout_factor=5, disable_stdout=False, mutate_covered=False, mutation_number=None):
         super().__init__(views)
@@ -59,6 +68,7 @@ class MutationController(views.ViewNotifier):
         self.mutation_number = mutation_number
         self.store_init_modules()
 
+    # Runs mutation controller.If original code tests or module load fails, it notifies it and and ends program with an error code.
     def run(self):
         self.notify_initialize(self.target_loader.names, self.test_loader.names)
         try:
@@ -71,7 +81,9 @@ class MutationController(views.ViewNotifier):
         except utils.ModulesLoaderException as error:
             self.notify_cant_load(error.name, error.exception)
             sys.exit(-2)
-
+    
+    # Used to run mutation process in method self.run()
+    # It loads test modules, starts the timer, creates new mutation score, and mutates all the modules
     def run_mutation_process(self):
         try:
             test_modules, total_duration, number_of_tests = self.load_and_check_tests()
@@ -85,7 +97,13 @@ class MutationController(views.ViewNotifier):
                 self.mutate_module(target_module, to_mutate, total_duration)
         except KeyboardInterrupt:
             pass
-
+        
+	# Loads and checks modules coming from self.test_loader.load() of this object
+	# In case tests didn't succeeded, it raises controller.TestsFailAtOriginal exception
+	# Returns values as a tuple:
+	# 1 test_modules - list of tuples containing in order: test module, target test, duration of the test
+    # 2 total duration - sum of completion times of all tests made on the original code
+	# 3 number_of_tests - amount of all targets tests
     def load_and_check_tests(self):
         test_modules = []
         number_of_tests = 0
@@ -101,6 +119,12 @@ class MutationController(views.ViewNotifier):
 
         return test_modules, total_duration, number_of_tests
 
+	# Runs tests for given two modules test_module, and returns results
+	# test_module - module with tests
+	# target_test - module with code to test
+	# Returns values as a tuple:
+	# 1 unittest.result - it contains test result
+	# 2 duration of the test.
     def run_test(self, test_module, target_test):
         suite = self.get_test_suite(test_module, target_test)
         result = unittest.TestResult()
@@ -109,12 +133,25 @@ class MutationController(views.ViewNotifier):
             suite.run(result)
         return result, timer.stop()
 
+	# loads tests from test module. If target_test is empty, then it loads all the tests.
+	# test_module - module with tests
+	# target_test - module with code to test	
+    # Returns:
+	# unittest.suite.TestSuite, which contains tests from test_module
     def get_test_suite(self, test_module, target_test):
         if target_test:
             return unittest.TestLoader().loadTestsFromName(target_test, test_module)
         else:
             return unittest.TestLoader().loadTestsFromModule(test_module)
 
+	# Mutates the module. It first converst target code to ast node tree.
+	# Then - if user enabld it - injects coverage into said tree.
+	# then it takes it's own mutant_generator (which can be one or higher order mutator)
+	# and for every possible set of mutations of the ast node tree
+	# runs tests for said mutation and saves result in score.
+	# target_module - module with loaded code to mutate
+	# to_mutate - (strangely, it is None type)
+	# test_modules - a list of modules with tests
     @utils.TimeRegister
     def mutate_module(self, target_module, to_mutate, total_duration):
         target_ast = self.create_target_ast(target_module)
@@ -135,6 +172,15 @@ class MutationController(views.ViewNotifier):
             else:
                 self.score.inc_incompetent()
 
+	# Injects coverage into the module. That means it creates a CoverageInjector from coverage.py
+	# which injects coverage into the ast node tree, creating a covered module.
+	# Then this covered module goes through tests, where it get's covered so
+	# nodes that were not participating in the test are not going to be mutated
+	# target_ast - ast node tree of the target_module
+	# target_module - module of code to mutate
+	# Returned values as a touple:
+	# 1 created coverage injector (or None if code is not going to be covered)
+	# 2 result of tests on covered code (or None if code is not going to be covered)
     def inject_coverage(self, target_ast, target_module):
         if not self.mutate_covered:
             return None, None
@@ -145,12 +191,27 @@ class MutationController(views.ViewNotifier):
         with self.stdout_manager:
             suite.run(coverage_result)
         return coverage_injector, coverage_result
-
+	
+	# Creates an ast node tree out of given module
+	# It is a result of parsed code visited by utils.ParentNodeTransformer
+	# it creates "parent" and "children" attributes in nodes
+	# "parent" is True or False, determining if a node a parent or not
+	# "children" is a list of node children
+	# target_module - module to turn into ast node tree
+	# Returned value:
+	# Created ast node tree
     @utils.TimeRegister
     def create_target_ast(self, target_module):
         with open(target_module.__file__) as target_file:
             return utils.create_ast(target_file.read())
-
+			
+	# Using mutant_ast, it creates a mutated module.
+	# If creating the module throws an exception (for example created code doesn't parse)
+	# then it throws an exception, and notifies an incompetent mutant
+	# target_module - module with code to mutate
+	# mutant_ast - ast node tree that has been mutated
+	# Returned value:
+	# Mutated module (or None in case of exception)
     @utils.TimeRegister
     def create_mutant_module(self, target_module, mutant_ast):
         try:
@@ -162,7 +223,14 @@ class MutationController(views.ViewNotifier):
         except BaseException as exception:
             self.notify_incompetent(0, exception, tests_run=0)
             return None
-
+	# Creates a test suite so that module can be tested, adds all tests from the tests_modules list.
+	# It takes coverage injection upon consideration.
+	# Sums all tests durations.
+	# tests_modules - list of modules of tests
+	# mutant_module - mutated module of code to test
+	# Returns values as a touple:
+	# 1 created suite
+	# 2 sum of all tests durations
     def create_test_suite(self, mutant_module):
         suite = unittest.TestSuite()
         utils.InjectImporter(mutant_module).install()
@@ -171,8 +239,12 @@ class MutationController(views.ViewNotifier):
             suite.addTests(self.get_test_suite(test_module, target_test))
         utils.InjectImporter.uninstall()
         return suite
-
-    def mark_not_covered_tests_as_skip(self, mutations, coverage_result, suite):
+		
+	# Marks not covered (ones that give Error type exception when iterated?) nodes as skip.
+    # mutations - list of operators.Mutation type. It is ast node that was mutated by operator.
+	# coverage_result - coverage.CoverageTestResult. Result of testing the code after covering it. 
+	# suite - unittest.suite.TestSuite. Test suite for this mutant.
+	def mark_not_covered_tests_as_skip(self, mutations, coverage_result, suite):
         mutated_nodes = {mutation.node.marker for mutation in mutations}
 
         def iter_tests(tests):
@@ -188,7 +260,11 @@ class MutationController(views.ViewNotifier):
                 setattr(test, test._testMethodName, unittest.skip('not covered')(test_method))
 
         iter_tests(suite)
-
+	# Runs test on mutant module. Updates score and otifies views.
+	# total_duration - duration of all tests
+	# mutant_module - module with mutant
+	# mutations - list of operators.Mutation type. It is ast node that was mutated by operator.
+	# coverage_result - coverage.CoverageTestResult. Result of testing the code after covering it. 
     @utils.TimeRegister
     def run_tests_with_mutant(self, total_duration, mutant_module, mutations, coverage_result):
         suite = self.create_test_suite(mutant_module)
@@ -199,7 +275,13 @@ class MutationController(views.ViewNotifier):
         timer.stop()
         self.update_score_and_notify_views(result, timer.duration)
 
-    def run_mutation_test_runner(self, suite, total_duration):
+	# Runs test from test suite. If time taken by the test is longer than certain factor,
+	# then test times out.
+	# suite - test suite
+	# total_duration - sum of durations of all module tests
+    # returned value:
+	# 1 result - result of the test suite
+	def run_mutation_test_runner(self, suite, total_duration):
         live_time = self.timeout_factor * (total_duration if total_duration > 1 else 1)
         test_runner_class = utils.get_mutation_test_runner_class()
         test_runner = test_runner_class(suite=suite)
@@ -208,7 +290,9 @@ class MutationController(views.ViewNotifier):
             result = test_runner.get_result(live_time)
             test_runner.terminate()
         return result
-
+	# Updates score and notifies views depending on result.
+	# result - suite test result
+	# mutant_duration - sum of durations of all module tests
     def update_score_and_notify_views(self, result, mutant_duration):
         if not result:
             self.update_timeout_mutant(mutant_duration)
@@ -219,28 +303,41 @@ class MutationController(views.ViewNotifier):
         else:
             self.update_killed_mutant(result, mutant_duration)
 
+	# Creates an update on information about timeout mutant
+	# duration - duration of all the tests
     def update_timeout_mutant(self, duration):
         self.notify_timeout(duration)
         self.score.inc_timeout()
-
+	
+	# Creates an update on information about incompetent mutant
+	# result - test suite result
+	# duration - duration of all the tests
     def update_incompetent_mutant(self, result, duration):
         self.notify_incompetent(duration, result.exception, result.tests_run)
         self.score.inc_incompetent()
 
+	# Creates an update on information about survived mutant
+	# result - test suite result
+	# duration - duration of all the tests
     def update_survived_mutant(self, result, duration):
         self.notify_survived(duration, result.tests_run)
         self.score.inc_survived()
 
+	# Creates an update on information about killed mutant
+	# result - test suite result
+	# duration - duration of all the tests
     def update_killed_mutant(self, result, duration):
         self.notify_killed(duration, result.killer, result.exception_traceback, result.tests_run)
         self.score.inc_killed()
 
+	# Initiates self.init_modules during initiation
     def store_init_modules(self):
         test_runner_class = utils.get_mutation_test_runner_class()
         test_runner = test_runner_class(suite=unittest.TestSuite())
         test_runner.start()
         self.init_modules = list(sys.modules.keys())
 
+	# Removes loaded modules from sys.modules dictionary.
     def remove_loaded_modules(self):
         for module in list(sys.modules.keys()):
             if module not in self.init_modules:
